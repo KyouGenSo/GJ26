6000f36c72c5f58107815153b549b8d7f038ee86
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2026-09-03 18:25:35 +0900

    エンジンを最新版に更新しました (#8)
    これに伴ういくつかの修正を行いました。

--- Game/Scripts/Scene/FactoryGJ26.h
+++ Game/Scripts/Scene/FactoryGJ26.h
@@ -13,6 +13,5 @@ enum SceneListGJ26 {
 class FactoryGJ26 final : public szg::BaseSceneFactory {
 
 public:
-	std::unique_ptr<szg::Scene> initialize_scene2() override;
-	std::unique_ptr<szg::Scene> create_scene2(i32 next) override;
+	std::unique_ptr<szg::Scene> create_scene2(i32 next) const override;
 };
